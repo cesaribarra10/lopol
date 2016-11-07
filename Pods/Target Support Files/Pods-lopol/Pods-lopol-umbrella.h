e81261e47446d0608540163d6c2d3e4570611592
Cesar Ibarra <cfi2103@columbia.edu>
2016-11-06 19:08:29 -0500

    initial commit

---
+++ Pods/Target Support Files/Pods-lopol/Pods-lopol-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_lopolVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_lopolVersionString[];
+
---
+++ lopol/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  lopol
+//
+//  Created by Cesar Ibarra on 11/6/16.
+//  Copyright © 2016 Cesar Ibarra. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseURL = "https://www.googleapis.com"
+    static let getRepresentatives = "/civicinfo/v2/representatives"
+    static let APIKey = "AIzaSyDVjVpwWAq9I3qOUAChddsvnc_W7iMVjO0"
+}
